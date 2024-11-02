073396eefce76185463e314d78340da9034dc149
ARottenberger <69944020+iplayer1337fivem@users.noreply.github.com>
2024-11-02 12:45:37 +0100

    diverse opdatering

--- shared/config.lua
+++ shared/config.lua
@@ -1,21 +1,14 @@
 Config = {}
-Config.Debug = true
+Config.Debug = false
 Config.InventoryImage = "ox_inventory/web/images/"  -- Source of inventory images (QB INVENTORY: "qb-inventory/html/images/")
 Config.ClosedShops = {
     [1] = {
         job = "police",
         coords = vec4(200.72, -235.76, 53.0, 300.26),
-        label = "Open Shop",
+        label = "Ånen butik",
         weight = 999999,
         allowedItems = {
             "water",
-            "weed_amnesia_baggy",
-            "weed_zkittlez_baggy",
-            "weed_ogkush_baggy",
-            "weed_purplehaze_baggy",
-            "weed_skunk_baggy",
-            "weed_whitewidow_baggy",
-            "weed_gelato_baggy",
         },
         targetDistance = 1.5,
     }
